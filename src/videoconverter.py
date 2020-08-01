318dc80e11872d839c504d9af488bf8462e6e7a7
xam4lor <xam4lor@gmail.com>
2020-08-01 13:22:46 +0200

    Added video generation based on given framerate.

--- src/videoconverter.py
+++ src/videoconverter.py
@@ -1,14 +1,16 @@
 import json
 import sys
+import os
 
 from binascii import a2b_base64
 
 
 def setup(fileName):
     print(
-          "\nStarting video creation algorithm."
+          "\nStarting video creation algorithm. This only works with the blocking frame option set in pSEngine."
         + "\nThere are 3 steps in order to complete this."
         + "\nPlease do not quit until everything is done or there might by issues on your final video."
+        + "\nMore infos on pSEngine at 'pSEngine.mecanicascience.fr' (copyright : 2020 - MecanicaScience)"
     )
 
     # Open the current recorder file
@@ -17,9 +19,12 @@ def setup(fileName):
     # Encoded string to PNG image stored in 'raw/' folder
     convertStringToPNG(jsonDatas['datas']['pixels']['pixelDatas'])
 
+    # Create video from the set of every created images
+    createVideoFromImageSet(jsonDatas['config']['drawFrameCount'])
+
     print(
           "\n\nEnd of program."
-        + "\nVideo has correctly been created."
+        + "\nVideo has correctly been created in 'datas/output.mp4'."
     )
 
 
@@ -36,7 +41,7 @@ def openRecorderFile(fileName):
 
 
 def convertStringToPNG(dataArray):
-    print("\n\n===== Processing images (2/3) =====\n")
+    print("\n\n===== Processing images (2/3) =====")
     for i in range(-1, len(dataArray)):
         print("Processing image " + str(i + 1) + '/' + str(len(dataArray)))
 
@@ -52,11 +57,20 @@ def convertStringToPNG(dataArray):
 
         binary_data = a2b_base64((cleanStr + "=" * 4).split('data:image/png;base64,')[1])
 
-        fd = open('raw/image_' + str(i) + '.png', 'wb')
+        fd = open('raw/image_' + '{:06d}'.format(i) + '.png', 'wb')
         fd.write(binary_data)
         fd.close()
 
-    print("\n===== END Processing images (2/3) =====")
+    print("===== END Processing images (2/3) =====")
+
+
+
+def createVideoFromImageSet(FPS):
+    print("\n\n===== Generating video from the set of images (3/3) =====\n...")
+
+    os.system('ffmpeg -framerate {0} -i raw/image_%06d.png datas/output.mp4'.format(FPS))
+
+    print("===== END Generating video from the set of images (3/3) =====")
 
 
 

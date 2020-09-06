91dbdc338f06c9206e7bb3e121c49e225178b9b7
xam4lor <xam4lor@gmail.com>
2020-09-06 17:27:37 +0200

    Updated run.bat and gitignore

--- run.bat
+++ run.bat
@@ -1,3 +1,4 @@
 % Modify the String inside " " with the name of your data file (without the '.json').
 % The file must by located inside the 'datas/' folder.
 py src/videoconverter.py "json-datas"
+PAUSE
